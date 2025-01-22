01e1c29841e1c9cd28ee87d3e9bbaf61371eac33
Marcos Schlick <marcosschlick@gmail.com>
2025-01-22 13:42:33 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -26,7 +26,7 @@ public class GameMinDTO {
     public GameMinDTO(GameMinProjection entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
-        this.year = entity.getYear();
+        this.year = entity.getGameYear();
         this.imgUrl = entity.getImgUrl();
         this.shortDescription = entity.getShortDescription();
     }
